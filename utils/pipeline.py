c47426b5a849d4fe1ee5ff009e10ca48dffa532c
Mengdi Wang <wangmengdi0701@hotmail.com>
2022-04-09 18:48:06 +0200

    add pipeline for 3D NST with per-layer operation on BNST

--- utils/pipeline.py
+++ utils/pipeline.py
@@ -675,3 +675,197 @@ def pipeline_3D_NST_simultaneous(   org_mesh,
             rendering_at_iter["rendering_iter" + str(i)] = new_rendering_rgba
 
     return verts_offsets, verts_colors, loss_history_reshaping, loss_history_texturing, rendering_at_iter
+
+
+def pipeline_3D_NST_OpsOnBNST(org_mesh,
+                    style_img,
+                    optim_type = 'reshaping',
+                    optim_init = None,
+                    rendering_size = (512,512),
+                    style_layers = style_layers_default,
+                    style_weights = style_weights_default,
+                    n_views_per_iter = 1,
+                    cameras = None,
+                    sampling_cameras = True,
+                    elevs = torch.tensor([0]),
+                    azims = torch.tensor([0]),
+                    perspective_camera = True,
+                    camera_dist = 2.7,
+                    faces_per_pixel = 50,
+                    n_iterations = 500, 
+                    learning_rate = 1e-5,
+                    plot_period = [10, 50, 100, 200, 500],
+                    model_pooling = 'max',
+                    clamping = False,
+                    indices = None,
+                    mean_coef = 1, mean_bias = 0, mean_freq_lower = None, mean_freq_upper = None,
+                    std_coef = 1, std_bias = 0, std_freq_lower = None, std_freq_upper = None):
+    """
+    Pipeline for running 3D neural style transfer, either reshaping or texturing. This function allows per-layer 
+    operations on batch normalization statistics such as affine transformation or FFT filter. Compared to 
+    pipeline_3D_NST, this function is simplied in the sense of ignoring mask.
+    Arguments:
+        org_mesh: 3D mesh object
+        style_img: tensor of style image of shape (1,3,h,w)
+        optim_type: 'reshaping' or 'texturing', string
+        optim_init: whether to have some initial value for per-vertex color during texturing
+        rendering_size: image size of rendering
+        style_layers: list of style layer names, e.g. ['conv1_1', 'conv3_1']
+        style_weights: list of weights attached to each style layer
+        n_views_per_iter: how many camera views are used for optimization per iteration, int
+        cameras: list of PyTorch3D cameras, will be generated if not given
+        sampling_cameras: whether to Poisson disc sampling for camera positions, boolean
+        elevs: camera elevations, will be ignored if sampling_cameras is True
+        azims: camera azimuths, will be ignored if sampling_cameras is True
+        perspective_camera: whether to use perspective camera or orthographical camera, boolean
+        camera_dist: distance of camera to mesh center (0,0,0)
+        faces_per_pixel: number of faces per pixel track along depth axis
+        n_iterations: number of optimization iterations
+        learning_rate: optimizer learning rate
+        plot_period: at which iteration to plot rendering and save mesh object
+        model_pooling: type of pooling layer in style model, can be 'max' or 'avg'
+        clamping: whether to clamp per-vertex color in range [0,1] in case of texturing
+        indices: subset of channels to consider w.r.t. BNST loss
+        *coef and *bias: params for affine transformation, e.g. x --> x * x_coef + x_bias
+    Returns:
+        what_to_optimize: per-vertex position offset or per-vertex color depending on task type
+        cameras: generated camera, may be reused in case of sequential reshaping and texturing
+        loss_history: dictionary of loss history
+        rendering_at_iter: list stores renderings at plot_period
+
+    """
+  
+    # normalize mesh
+    center, scale = mesh_normalization(org_mesh)
+
+    # optimizer and the tensor to be optimized
+    # what_to_optimize: per-vertex position offset in case of reshaping and per-vertex color in case of texturing 
+    verts_shape = org_mesh.verts_packed().shape
+    if optim_type == 'reshaping':
+        if optim_init is None:
+            what_to_optimize = torch.full(verts_shape, 0.0, device=device, requires_grad=True)
+        else:
+            what_to_optimize = optim_init.view(verts_shape[0], 3)
+            what_to_optimize /= 1e2 # offsets should be smaller
+            what_to_optimize = what_to_optimize.detach()
+            what_to_optimize.requires_grad = True
+    elif optim_type == 'texturing':
+        if optim_init is None:
+            what_to_optimize = torch.full([1, verts_shape[0], 3], 0.5, device=device, requires_grad=True)
+            # what_to_optimize = torch.rand(1, verts_shape[0], 3, device=device, requires_grad=True)
+        else:
+            what_to_optimize = optim_init.view(1, verts_shape[0], 3)
+            what_to_optimize = (what_to_optimize - what_to_optimize.min()) / (what_to_optimize.max() - what_to_optimize.min()) # normalize to [0,1]
+            what_to_optimize = what_to_optimize.detach() # make leaf tensor
+            what_to_optimize.requires_grad = True
+        org_mesh.textures = TexturesVertex(verts_features = what_to_optimize)
+    else:
+        raise RuntimeError("Either reshaping or texturing! But now it is: " + optim_type)
+    optimizer = torch.optim.Adam([what_to_optimize], lr=learning_rate)
+
+    # get renderer, cameras and lights
+    renderer = get_renderer(rendering_size = rendering_size, faces_per_pixel = faces_per_pixel, sil_shader = optim_type == 'reshaping')
+    lights = get_lights()
+    if cameras is None: # cameras for reshaping should be re-used for texturing
+        print("no cameras are given, and sampling_cameras is", sampling_cameras)
+        cameras = get_cameras(sampling_cameras = sampling_cameras, elevs = elevs, azims = azims, perspective_camera = perspective_camera, camera_dist = camera_dist)
+    else:
+        print("cameras are already given, reusing the given cameras")
+    camera_visual = get_visual_camera(perspective_camera = perspective_camera, camera_dist = camera_dist)
+    n_views_per_iter = min(n_views_per_iter, len(cameras))
+    print("number of cameras:", len(cameras))
+    # print("n_views_per_iter is:", n_views_per_iter)
+    
+    # get some initial renderings
+    org_rendering_rgba = get_rgba_rendering(org_mesh, renderer, camera_visual, lights).detach().cpu()
+    
+    # VGG with style loss layers
+    model_style, style_losses = get_models_OpsOnBNST(   style_img, 
+                                                        style_layers, 
+                                                        model_pooling = model_pooling,
+                                                        silent =True,
+                                                        indices = indices,
+                                                        mean_coef = mean_coef, mean_bias = mean_bias, mean_freq_lower=mean_freq_lower, mean_freq_upper=mean_freq_upper,
+                                                        std_coef = std_coef, std_bias = std_bias, std_freq_lower=std_freq_lower, std_freq_upper=std_freq_upper
+                                                        )
+    
+    # loss history
+    loss_history = {name: {'weight':1.0, 'values':[]} for name in ['mean_loss', 'std_loss']}
+        
+    # return value, which keeps renderings at specified iterations
+    rendering_at_iter = {}
+    
+    # optimization loop
+    loop = tqdm(range(1, n_iterations + 1))
+    for i in loop:
+        if clamping and optim_type == 'texturing':
+            with torch.no_grad():
+                what_to_optimize.clamp_(0., 1.)
+        
+        optimizer.zero_grad()
+        
+        # update mesh
+        if optim_type == 'reshaping':
+            new_mesh = org_mesh.offset_verts(what_to_optimize)
+        else: # optim_type == 'texturing'
+            new_mesh = org_mesh
+            new_mesh.textures = TexturesVertex(verts_features = what_to_optimize) 
+
+        # a dictionary saves style losses at this iteration
+        loss_current_iter = {name:0 for name in ['mean_loss', 'std_loss']}
+        
+        for j in np.random.permutation(len(cameras)).tolist()[:n_views_per_iter]:
+            # get data tensors
+            rendering_rgba = get_rgba_rendering(new_mesh, renderer, cameras[j], lights) 
+            rendering = rendering_rgba[..., 3] if optim_type == 'reshaping' else rendering_rgba[..., :3]
+            rendering_tensor = tensor_loader(rendering)
+            
+            # forward pass
+            model_style(rendering_tensor)
+
+            # sum up style losses over all style loss layers
+            for sl, sl_weight in zip(style_losses, style_weights):
+                for name in ['mean_loss', 'std_loss']:
+                    loss_current_iter[name] += sl.losses[name] * sl_weight
+
+        # average loss among n_views_per_iter    
+        for name in ['mean_loss', 'std_loss']:
+            loss_current_iter[name] /= n_views_per_iter
+        
+        # weighted sum of the losses
+        # !!! careful !!!
+        # style_loss_types: {style loss name : style loss weight}
+        # loss_history: {style/smoothing/regularization loss name : {'weight' : weight, values : [...] }}
+        # loss_current_iter: {style/smoothing/regularization loss name : value}
+        sum_loss = 0
+        for loss_name, loss_value in loss_current_iter.items():
+            weighted_loss = loss_value * loss_history[loss_name]['weight']
+            sum_loss += weighted_loss
+            loss_history[loss_name]['values'].append(weighted_loss.detach().cpu())
+        
+        # print losses
+        loop.set_description("total_loss = %.5f" % sum_loss)
+
+        # Optimization step
+        with NoStdStreams():        # run KeOps kernels silently
+            sum_loss.backward()
+        optimizer.step()
+        
+        # plot renderings
+        if i in plot_period:
+            with torch.no_grad():
+                new_rendering_rgba = get_rgba_rendering(new_mesh, renderer, camera_visual, lights).detach().cpu()
+            rgb = optim_type == 'texturing'
+            sil = optim_type == 'reshaping' or (optim_init is not None)
+            visualize_prediction(new_rendering_rgba = new_rendering_rgba, org_rendering_rgba = org_rendering_rgba, rgb = rgb, sil = sil, title="iter: %d" % i)
+                 
+            # save mesh
+            final_verts, final_faces = new_mesh.get_mesh_verts_faces(0)
+            final_verts = final_verts * scale + center
+            final_obj = "./runtime_objs/obj_iter" + str(i) + ".obj"
+            save_obj(final_obj, final_verts, final_faces)
+
+            # add rendering to dictionary
+            rendering_at_iter["rendering_iter" + str(i)] = new_rendering_rgba[..., 3] if optim_type == 'reshaping' else new_rendering_rgba[..., :3]
+
+    return what_to_optimize, cameras, loss_history, rendering_at_iter
